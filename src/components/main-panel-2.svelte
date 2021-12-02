8547808adb3ff76c94c65a034ad366de440f5176
Ajay Rajamani <ajay@ladderlife.com>
2021-12-01 19:17:01 -0800

    make twocol comp

--- src/components/main-panel-2.svelte
+++ src/components/main-panel-2.svelte
@@ -1,6 +1,9 @@
 <script>
     // This component is the second panel on the main page, with a light background picture, 
     // bullet points of our main talking points, and a pic of the proposed construction
+
+    //Imports
+    import TwoCol from "./twocolpanel.svelte";
 </script>
 
 <section>
@@ -13,24 +16,28 @@
                     <li>Reason wny the path needs to be changed</li>
                 </ul>
         </div>
-        <div class="column">
-            <div class="column-inner">
-            </div>
-        </div>
 
-        <div class="column">
-            <div class="column-inner">
-                <h2>Current Path Routing</h2>
+        <TwoCol>
+            <div class="column-inner" slot="col1">
+                <h2>Current Bike Path Design</h2>
                 <img src="" alt="" class="route-image">
                 <ul>
                     <li>Disadvantage of path</li>
                     <li>Disadvantage of path</li>
                     <li>Disadvantage of path</li>
                 </ul>
-
-                <h2> New path routing</h2>
             </div>
-        </div>
+
+            <div class="column-inner" slot="col2">
+                <h2>Alternate # 4 (preferred route)</h2>
+                <img src="" alt="" class="route-image">
+                <ul>
+                    <li>advantage of path</li>
+                    <li>advantage of path</li>
+                    <li>advantage of path</li>
+                </ul>
+            </div>
+        </TwoCol>
 </section>
 
 <style>
@@ -40,28 +47,26 @@
         display: flex;
         flex-direction: row;
         position: relative;
+        background-color: #DAE9F2;
         /* background-image: url("/assets/img/bates2.jpg");
         background-size: cover;
         background-position: center;
         opacity: 15%; */
     }
-    section::before {
+    /* section::before {
         content: "";
         position: absolute;
         top: 0px;
         right: 0px;
         bottom: 0px;
         left: 0px;
-        opacity: .4; 
+        opacity: .2; 
         z-index: -1;
         background-size: cover;
         background-position: center;
-        background-image: url("/assets/img/bates2.jpg");
-    }
+        background-image: url("../assets/img/bates3.jpg");
+    } */
 
-    .column {
-        width: 50%;
-    }
 
     .column-inner {
         display: flex;
--- src/components/twocolpanel.svelte
+++ src/components/twocolpanel.svelte
@@ -2,3 +2,17 @@
     // This component takes in children and arranges them in 2 columns
 </script>
 
+<div class="column">
+    <slot name="col1"></slot>
+</div>
+
+<div class="column">
+    <slot name="col2"></slot>
+</div>
+
+<style>
+    .column {
+        width:50%;
+    }
+</style>
+
